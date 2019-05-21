1e3a09e45a45efa317219c2e8712d43cdbfebb5b
SeLiNnnn丶Y <selin.cd@foxmail.com>
2019-05-22 00:38:01 +0800

    first commit my assignment

--- main/main.js
+++ main/main.js
@@ -1,4 +1,9 @@
 // Write your cade below:
-module.exports = function main() {
-  
-};
+module.exports = function main(a, b) {
+  // console.log(a - Math.floor(a / b) * b)
+  if (b === 0) {
+    console.log("除数不能为0")
+  } else {
+    return a - Math.floor(a / b) * b
+  }
+}
