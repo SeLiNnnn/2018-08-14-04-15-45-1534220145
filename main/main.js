518cd6d39109ca362850957ca4f609806b17426e
SeLiNnnn丶Y <selin.cd@foxmail.com>
2019-05-28 20:26:31 +0800

    update the code

--- main/main.js
+++ main/main.js
@@ -1,9 +1,11 @@
 // Write your cade below:
 module.exports = function main(a, b) {
-  // console.log(a - Math.floor(a / b) * b)
-  if (b === 0) {
-    console.log("除数不能为0")
-  } else {
-    return a - Math.floor(a / b) * b
-  }
+  return a % b
+
+  // 我还以为不能直接用%来计算...
+  // if (b === 0) {
+  //   console.log("除数不能为0")
+  // } else {
+  //   return a - Math.floor(a / b) * b
+  // }
 }
